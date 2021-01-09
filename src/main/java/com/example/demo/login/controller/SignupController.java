0a9f29d61a413871f239cba7802fe3bee8c54c32
金田尚大 <1111510078r21@gmail.com>
2021-01-09 20:51:16 +0900

    signupのコントローラー実装

--- src/main/java/com/example/demo/login/controller/SignupController.java
+++ src/main/java/com/example/demo/login/controller/SignupController.java
@@ -1,5 +1,37 @@
 package com.example.demo.login.controller;
 
-public class SignupController {
+import java.util.LinkedHashMap;
+import java.util.Map;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class SignupController {
+	private Map<String, String> radioMarriage;
+
+	@GetMapping("/signup")
+	public String getSignUp(Model model) {
+		radioMarriage = initRadioMarriage();
+
+		model.addAttribute("radioMarriage", radioMarriage);
+
+		return "login/signup";
+	}
+
+	@PostMapping("/signup")
+	public String postSignUp(Model model) {
+		return "redirect:/login";
+	}
+
+	private Map<String, String> initRadioMarriage() {
+		Map<String, String> radioMap = new LinkedHashMap<>();
+
+		radioMap.put("既婚", "true");
+		radioMap.put("未婚", "false");
+
+		return radioMap;
+	}
 }
