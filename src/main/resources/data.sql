832bc67268ad1d1e9003b13abd71cf6eb2a0d995
金田尚大 <1111510078r21@gmail.com>
2021-01-13 11:33:48 +0900

    ユーザー関連のsql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO employee(employee_id,employee_name,age) VALUES(1,'山田太郎',30);
+INSERT INTO m_user(user_id,password,user_name,birthday,age,marriage,role) VALUES('yamada@xxx.co.jp','password','山田太郎','19900101',28,false,'ROLE_ADMIN');
