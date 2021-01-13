a93ebed462c023d975a17e45ec16add88189d41c
金田尚大 <1111510078r21@gmail.com>
2021-01-13 10:35:26 +0900

    aop arround使用

--- src/main/java/com/example/demo/login/aspect/LogAspect.java
+++ src/main/java/com/example/demo/login/aspect/LogAspect.java
@@ -1,21 +1,26 @@
 package com.example.demo.login.aspect;
 
-import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LogAspect {
-	@Before("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
-	public void startLogAspect(JoinPoint jp) {
+	@Around("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
+	public Object startLogAspect(ProceedingJoinPoint jp) throws Throwable {
 		System.out.println("method start: " + jp.getSignature());
-	}
 
-	@After("execution(* com.example.demo.login.controller.LoginController.getLogin(..))")
-	public void endLogAspect(JoinPoint jp) {
-		System.out.println("method end: " + jp.getSignature());
+		try {
+			Object result = jp.proceed();
+			System.out.println("method end: " + jp.getSignature());
+
+			return result;
+		} catch (Exception e) {
+			System.out.println("method abend: " + jp.getSignature());
+			e.printStackTrace();
+			throw e;
+		}
 	}
 }
