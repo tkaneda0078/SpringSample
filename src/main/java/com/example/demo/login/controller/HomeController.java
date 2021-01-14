eab980b5b7262d619db7432b4c1e102bb713c1c6
金田尚大 <1111510078r21@gmail.com>
2021-01-14 18:27:35 +0900

    ユーザー取得、カウント結果を表示

--- src/main/java/com/example/demo/login/controller/HomeController.java
+++ src/main/java/com/example/demo/login/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.example.demo.login.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.service.UserService;
 
 @Controller
@@ -18,6 +21,26 @@ public class HomeController {
 		model.addAttribute("contents", "login/home::home_contents");
 
 		return "login/homeLayout";
+	}
+
+	@GetMapping("/userList")
+	public String getUserList(Model model) {
+		model.addAttribute("contents", "login/userList :: userList_contents");
+
+		List<User> userList = userService.getAll();
+
+		model.addAttribute("userList", userList);
+
+		int count = userService.count();
+
+		model.addAttribute("userListCount", count);
+
+		return "login/homeLayout";
+	}
+
+	@GetMapping("/userList/csv")
+	public String getUserListCsv(Model model) {
+		return getUserList(model);
 	}
 
 	@PostMapping("/logout")
