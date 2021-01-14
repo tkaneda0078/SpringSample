51c5daffd9c37f4d174d0a8208d6ae432efe8682
金田尚大 <1111510078r21@gmail.com>
2021-01-14 18:00:27 +0900

    サービスからユーザー作成メソッド呼び出す

--- src/main/java/com/example/demo/login/domain/service/UserService.java
+++ src/main/java/com/example/demo/login/domain/service/UserService.java
@@ -3,10 +3,21 @@ package com.example.demo.login.domain.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.login.domain.model.User;
 import com.example.demo.login.domain.repository.UserDao;
 
 @Service
 public class UserService {
 	@Autowired
 	UserDao dao;
+
+	public boolean create(User user) {
+		int rowNumber = dao.createOne(user);
+
+		if (rowNumber > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
